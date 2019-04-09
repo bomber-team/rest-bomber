9536775275978b5c0de675be84a25744955183f7
kostya05983 <kostya05983@mail.ru>
2019-04-09 16:54:32 +0700

    [ADD] parser with different readers

---
+++ parser/readers.go
@@ -0,0 +1,39 @@
+package parser
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Reader interface {
+	read(path string) string
+}
+
+type FileReader struct {
+}
+
+//Read file from disk
+//returns string for future parsing
+func (reader FileReader) read(path string) string {
+	// Open our jsonFile
+	jsonFile, err := os.Open(path)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully open" + path)
+	// defer the closing of our jsonFile so that we can Parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	return string(byteValue)
+}
+
+type ApiReader struct{}
+
+//Read scenario from api, path address of server
+//returns string for future parsing
+func (reader ApiReader) read(path string) string {
+	return ""
+}
