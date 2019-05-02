8969c7f503c2119c1351a34f21decb1ec8415b7b
kubitre <kubitre@gmail.com>
2019-05-02 13:50:48 +0700

    [REF] Remove comments, update comments and updated method return type in interval

--- parser/readers.go
+++ parser/readers.go
@@ -6,17 +6,18 @@ import (
 	"os"
 )
 
+/*Reader - main interface for read datas from other sources*/
 type Reader interface {
-	read(path string) string
+	read(path string) []byte
 }
 
+/*FileReader - main structure for read files from local disk schemes*/
 type FileReader struct {
 }
 
 //Read file from disk
 //returns string for future parsing
-func (reader *FileReader) read(path string) []byte {
-	// Open our jsonFile
+func (reader FileReader) read(path string) []byte {
 	jsonFile, err := os.Open(path)
 	// if we os.Open returns an error then handle it
 	if err != nil {
