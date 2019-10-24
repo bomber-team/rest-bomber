54c6d94dad30f207d12115e62e5a88e3be83fec5
kubitre <kubitre@gmail.com>
2019-10-25 04:01:58 +0700

    update app and add speaker producer for discovering

--- application/app.go
+++ application/app.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"github.com/bomber-team/rest-bomber/core"
+	"github.com/bomber-team/rest-bomber/routes"
 	"github.com/gorilla/mux"
 )
 
@@ -10,6 +11,14 @@ type Application struct {
 	Router *mux.Router
 }
 
-func newApplication() *Application {
-	return &Application{}
+func NewApplication() *Application {
+	return &Application{
+		Core:   core.InitCore(),
+		Router: mux.NewRouter(),
+	}
+}
+
+func newRouter() *mux.Router {
+	routes := &routes.Routes{}
+	return mux.NewRouter()
 }
