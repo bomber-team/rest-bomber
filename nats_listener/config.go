31a8fa22d3c3b4603964086235575c8467946a31
Anosov Konstantin <32496864+kubitre@users.noreply.github.com>
2020-12-09 00:24:19 +0700

    Update config.go

--- nats_listener/config.go
+++ nats_listener/config.go
@@ -12,7 +12,7 @@ type NatsConnectionConfiguration struct {
 	MaxWait          int          `cf_env:"NATS_MAX_WAIT" cf_default:"1"`
 	ReconnectDelay   int64        `cf_env:"NATS_RECONNECT_DELAY" cf_default:"2"`
 	CurrentServiceID string       `cf_env:"BOMBER_ID" cf_default:"15123kjnsjhad"`
-	LogLevel         logrus.Level `cf_env:"LOG_LEVEL" cf_default:"error"`
+	LogLevel         string `cf_env:"LOG_LEVEL" cf_default:"error"`
 }
 
 func ParseConfiguration() (*NatsConnectionConfiguration, error) {
